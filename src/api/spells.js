736c12ba1d705c2585c3a8b14c6e6271b88b4b7a
goingpaper <atonkin98@gmail.com>
2023-03-08 17:51:04 +0800

    use https in spells/id api

--- src/api/spells.js
+++ src/api/spells.js
@@ -3,5 +3,5 @@ exports.getSpells = () =>
         .then(response => response.json());
 
 exports.getSpell = (spellIndex) =>
-    fetch(`http://www.dnd5eapi.co/api/spells/${spellIndex}`)
+    fetch(`https://www.dnd5eapi.co/api/spells/${spellIndex}`)
         .then(response => response.json());
